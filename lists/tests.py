9aef6bb2899150b4cbbfa5dba97d6814648aef33
omfg <podsh74@gamil.com>
2019-10-16 14:50:21 +0500

    Тестовый клиент Django без его использования

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 
 
 
@@ -12,11 +12,12 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
     
-    def test_home_page_return(self):
+    def test_home_page_return_correct_html(self):
         '''ТЕСТ: домашняя страница возвращает правильный html'''
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
+    
+   
