6ff832c73f13d2fa4d42ed8f6217a93d62104a02
omfg <podsh74@gamil.com>
2019-10-18 11:00:08 +0500

    Добавили модели базы данных

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 
 
@@ -16,5 +17,24 @@ class HomePageTest(TestCase):
         '''Тест: сохранения POST запроса'''
         responce = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', responce.content.decode())
-        self.assertTemplateUsed(responce, 'home.html')
-   
+
+
+class ItemModelTest(TestCase) :
+    '''Тест модели элемента списка'''
+
+    def test_saving_and_retrieving_items(self):
+        '''тест сохранения и получения элементов списка'''
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(), 2)
+
+        first_saved_item = saved_item[0]
+        second_saved_item = saved_item[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
