8fd86c1d657a5aa6ce3e5bfbb97476a96cfbc27f
omfg <podsh74@gamil.com>
2019-11-05 17:58:49 +0500

    неполадки с views.py

--- lists/tests.py
+++ lists/tests.py
@@ -33,6 +33,16 @@ class HomePageTest(TestCase):
         self.assertEqual(responce.status_code, 302)
         self.assertEqual(responce['location'], '/')
 
+    def test_displays_all_list_items(self):
+        '''тест: отражаются все элементы списка'''
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        responce = self.client.get('/')
+
+        self.assertIn('item 1', responce.content.decode())
+        self.assertIn('item 2', responce.content.decode())
+
 class ItemModelTest(TestCase) :
     '''Тест модели элемента списка'''
 
