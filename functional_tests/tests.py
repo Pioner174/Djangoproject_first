5f5b96012d6965d7394495ed3a01c27ce4e50be8
omfg <podsh74@gamil.com>
2019-11-06 14:42:48 +0500

    functional_tests.py переименован и вынесен в папку + запускается из тестов django

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
     '''тест нового посетитиля'''
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''тест: можно начать список и получитьл его позже!!!!'''
-        self.browser.get('http://localhost:8000/')
+        self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -40,9 +41,6 @@ class NewVisitorTest(unittest.TestCase):
         self.cheak_row_in_table('2: Сделать мушку из павлиньих перьев')
         self.fail('Закончить тест')
 
-
-if __name__=='__main__':
-    unittest.main(warnings='ignore')
 
 # browser = webdriver.Firefox()
 # assert 'To-Do' in browser.title
