181d513734342eb77ad04beeb2a1468fb4b383de
omfg <podsh74@gamil.com>
2019-11-06 16:13:09 +0500

    конец 6 главы

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
 import time
+from selenium.common.exceptions import WebDriverException
 
+MAXX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     '''тест нового посетитиля'''
@@ -18,6 +20,21 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+    def wait_for_row_in_list_table(self,row_text):
+        '''ожидает строку в таблице списка'''
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAXX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''тест: можно начать список и получитьл его позже!!!!'''
         self.browser.get(self.live_server_url)
@@ -31,14 +48,14 @@ class NewVisitorTest(LiveServerTestCase):
         )
         inputbox.send_keys('Купить павлиньи перья')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
-        self.cheak_row_in_table('1: Купить павлиньи перья')
+
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Сделать мушку из павлиньих перьев')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
-        self.cheak_row_in_table('1: Купить павлиньи перья')
-        self.cheak_row_in_table('2: Сделать мушку из павлиньих перьев')
+
+        self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
         self.fail('Закончить тест')
 
 
